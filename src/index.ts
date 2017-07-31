3bdda52c4b1168f24e834cd3482172000f919f13
Paul Baker <paul.baker@wgu.edu>
2017-07-31 02:59:56 -0600

    Adding dev package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // Include custom styles into the script to be injected into web pages.
 // Remove this line to reduce script output size if you do not need styles.
 import './index.css';
+import * as $ from 'jquery'; // This is a dev only dependency. The UserScript imports these with @require meta headers.
 
 const who = 'world';
 console.log(`Hello ${who}!`);
